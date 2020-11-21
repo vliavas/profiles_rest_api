7f386eed1e5e10d8642a36dd56f3b7a7d54e472c
vliavas@rogers.com <vliavas@rogers.com>
2020-11-20 21:25:28 -0500

    Configured new files

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstactBaseUser
+from django.contrib.auth.models import PermissionMixin
 
-# Create your models here.
+
+class UserProfile (AbstractBaseUser, PermissionMixin):
+    """Database model for users in the system """
+    email = models.EmailField(max_length=255, unique=True)
+    name=models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
