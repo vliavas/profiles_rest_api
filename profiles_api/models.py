51b92a8ab9557163d872bfecd1561eadf741b69e
vliavas@rogers.com <vliavas@rogers.com>
2020-11-21 21:44:15 -0500

    Enabled django superuser

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -19,13 +19,13 @@ class UserProfileManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
 
-        return users
+        return user
 
     def create_superuser(self, email, name, password):
         """Create and save new superuser with given details"""
         user = self.create_user(email, name, password)
 
-        user.is_superuse = True
+        user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
