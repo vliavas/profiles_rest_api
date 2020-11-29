4f0c438bcf3b3bb43ba4cb46f4ae5274bdfbcd6d
vliavas@rogers.com <vliavas@rogers.com>
2020-11-28 19:45:16 -0500

    Error LoginAPI

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 
 # Create your models here.
@@ -55,3 +56,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return string rep of our user"""
         return self.email
+
+# class ProfileFeedItem(modles.Model):
+#     """Profile status update"""
+#     user_profile = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#     status_text = models.CharField(max_length=255)
+#     create_on = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         """Return model as str"""
+#         return self.status_text
